4aae06a478b4d5cf79059fd312a43d962aa0d857
j-wu21 <jacckywu@gmail.com>
2024-03-12 23:14:30 -0500

    made a backend API

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+let groceryList = [];
+let cacheName = '';
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    const thing = {
+        firstName: cacheName,
+        lastName: 'bong',
+        dongSize: 10
+    };
+    res.json(thing);
+});
+
+app.post('/change-name', (req, res) => {
+    cacheName = req.body.newName;
+    res.send('name has been changed');
+});
+app.delete('/be-gone', (req, res) => {
+    cacheName = '';
+    res.send('it been goned');
+});
+
+
+//#region grocery actions
+app.get('/show', (req, res) => {
+    res.json(groceryList);
+    
+});
+
+app.post('/add', (req, res) => {
+    if (groceryList.includes(req?.body?.groceryItem)) {
+        res.send(req.body.groceryItem.toString() + ' already in the list');
+    }
+    else if (req?.body?.groceryItem.length > 0) {
+        groceryList.push(req.body.groceryItem);
+        res.send(req.body.groceryItem.toString() + ' added to list');
+    }
+    else {
+        res.send('can not add nothing');
+    }
+
+});
+
+app.delete('/clear', (req, res) => {
+    groceryList = [];
+    res.send('no more grocery list');
+});
+//#endregion
+
+
+app.listen(port, () => {
+    console.log(`running on port: ${port}`);
+});
+
+
+
